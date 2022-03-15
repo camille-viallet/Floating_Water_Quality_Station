d12c889ab2884819d82d35ce2782d515bf9c8437
camille-viallet <camille.viallet@etu.univ-grenoble-alpes.fr>
2022-03-15 14:09:50 +0100

    Removing the bin folder from the git

--- src/bin/lora-e5-dev/random/musl_lcg.d
+++
@@ -1,8 +0,0 @@
-/home/emeric/Documents/Projet_Floating_Water/code/src/bin/lora-e5-dev/random/musl_lcg.o: \
- /home/emeric/Documents/Projet_Floating_Water/RIOT/sys/random/musl_lcg.c \
- /home/emeric/Documents/Projet_Floating_Water/code/src/bin/lora-e5-dev/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h
-
-/home/emeric/Documents/Projet_Floating_Water/code/src/bin/lora-e5-dev/riotbuild/riotbuild.h:
-
-/usr/lib/gcc/arm-none-eabi/9.2.1/include/stdint.h:
